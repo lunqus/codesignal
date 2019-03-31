9e05cee2d73d053835066ab124e9739184fc86e5
LUNKUS <sigitas@lunkus.com>
2019-03-31 12:13:04 +0300

    well formatted

--- src/centuryFromYear/CenturyFromYearMain.java
+++ src/centuryFromYear/CenturyFromYearMain.java
@@ -3,21 +3,14 @@ package centuryFromYear;
 public class CenturyFromYearMain {
 
     int century(int year) {
-
-        if (year % 100 == 0) {
+        if (year % 100 == 0)
             return year / 100;
-        } else {
-           return year / 100 + 1;
-        }
-
-
+                return year / 100 + 1;
     }
-
 
     public static void main(String[] args) {
 
         CenturyFromYearMain century = new CenturyFromYearMain();
-
-        System.out.println(century.century(1999));
+        System.out.println(century.century(2005));
     }
 }
