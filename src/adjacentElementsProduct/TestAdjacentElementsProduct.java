41538f155edede9415404b8ace9ba4bf98e86004
LUNKUS <sigitas@lunkus.com>
2019-04-16 22:14:59 +0300

    Adjacent Elements Product

--- src/adjacentElementsProduct/TestAdjacentElementsProduct.java
+++ src/adjacentElementsProduct/TestAdjacentElementsProduct.java
@@ -1,4 +1,26 @@
 package adjacentElementsProduct;
 
 public class TestAdjacentElementsProduct {
+
+    public static void main(String[] args) {
+
+        int res;
+        int[] inputArray = {1,3,5,7,9,-3,5,2,6};
+
+        TestAdjacentElementsProduct aep = new TestAdjacentElementsProduct();
+        res = aep.adjacentElementProduct(inputArray);
+
+        System.out.println(res);
+    }
+
+    int adjacentElementProduct(int[] inputArray) {
+        int max = inputArray[0] * inputArray[1];
+
+        for (int i = 2; i < inputArray.length; i++) {
+            if (max < inputArray[i-1] * inputArray[i]) {
+                max = inputArray[i-1] * inputArray[i];
+            }
+        }
+        return max;
+    }
 }
